ca951549252b7ad820872587fb5f4dc674b7b61b
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-07-24 15:18:11 -0300

    feat: agrupando as images para usar

---
+++ src/assets/game_images.ts
@@ -0,0 +1,13 @@
+import Lizard from './images/icon-lizard.svg';
+import Paper from './images/icon-paper.svg';
+import Rock from './images/icon-rock.svg';
+import Scissors from './images/icon-scissors.svg';
+import Spock from './images/icon-spock.svg';
+
+export {
+    Lizard,
+    Paper,
+    Rock,
+    Scissors,
+    Spock
+}
---
+++ src/assets/layout_images.ts
@@ -0,0 +1,17 @@
+import bg_pentagon from './images/bg-pentagon.svg';
+import bg_triangle from './images/bg-triangle.svg';
+import icon_close from './images/icon-close.svg';
+import rules_lv1 from './images/image-rules.svg';
+import rules_lv2  from './images/image-rules-bonus.svg';
+import logo_lv1 from './images/logo.svg';
+import logo_lv2 from './images/logo-bonus.svg';
+
+export {
+    bg_pentagon,
+    bg_triangle,
+    icon_close,
+    rules_lv1,
+    rules_lv2,
+    logo_lv1,
+    logo_lv2
+}
