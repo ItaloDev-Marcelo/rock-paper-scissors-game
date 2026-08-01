4b9996c8188d9277ee59d4551875d3e6e6eeecad
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-01 15:53:00 -0300

    feat: creating RulesPaper and add style

--- src/components/RulesPapper.tsx
+++ src/components/RulesPapper.tsx
@@ -1,15 +1,16 @@
 import { rules_lv1, rules_lv2 } from "../assets/layout_images"
+import type { LevelType } from "../shared/types/CommunType"
 
-interface RulesPaperType {
-    level: number
+type RulesType = LevelType & {
+  changeRules :() => void
 }
 
-
-const RulesPapper = ({level}:RulesPaperType) => {
-  const paperImage = level == 1 ? rules_lv1 : rules_lv2
+const RulesPapper = ({level,changeRules}:RulesType) => {
+  const paperImage = level ? rules_lv1 : rules_lv2
   return (
-    <div>
-      <img src={paperImage} alt='rules-image' />
+    <div className="bg-amber-50 fixed w-screen h-screen top-0 flex flex-col items-center justify-center ">
+      <button className='fixed top-5 right-10 text-gray-400 text-2xl cursor-pointer' onClick={changeRules}>✖</button>
+      <img src={paperImage} alt='rules-image' className=' w-[75%] xl:w-[25%] left-[9%] md:left-[35%]' />
     </div>
   )
 }
