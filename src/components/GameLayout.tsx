2adc5bd9a7f7c33da90e8fc07b5ae2d07745e0b6
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-01 15:53:40 -0300

    feat: creating GameLayout

---
+++ src/components/GameLayout.tsx
@@ -0,0 +1,28 @@
+
+import { LevelLayout } from '../shared/constants/LevelLayout'
+import type { LevelType } from '../shared/types/CommunType'
+import { GameBtnOption } from './GameBtnOption'
+
+type GameLayoutType = LevelType & {
+  player: (num:number) => void
+}
+
+const GameLayout = ({level,player}:GameLayoutType) => {
+
+  const {finalLayout, game} = LevelLayout(level)
+  
+  return (
+    <div className="relative top-20 flex flex-col scale-75 xl:scale-90 items-center justify-center w-full h-75">
+    <img src={finalLayout} alt='' className='absolute h-75' />
+       <div className='relative'>
+        {
+        game.map((item,index) => (
+           <GameBtnOption key={index} value={item.value} name={item.name} from={item.from} to={item.to} icon={item.icon} scale='hover:scale-110 xl:hover:scale-120 xl:scale-110' player={player}  />
+        ))
+       }
+       </div>
+    </div>
+  )
+}
+
+export default GameLayout
