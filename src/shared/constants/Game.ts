9b27e55577ccd7d27a07b93f75dde9755449532e
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-01 15:50:47 -0300

    feat: creating GameIcons formate

---
+++ src/shared/constants/Game.ts
@@ -0,0 +1,72 @@
+import { Lizard, Paper, Rock, Scissors, Spock } from '../../assets/game_images'
+
+
+export const gameIcons1 = [
+    {
+        icon: Paper,
+        value: 1,
+        color: '',
+        name: 'icon1',
+        from: '#EDAA0E',
+        to: '#C76B1B'
+    },
+     {
+        icon: Scissors,
+        value: 2,
+        color: '',
+        name: 'icon2',
+        from: '#4865F4',
+        to: '#1F3CB8'
+    },
+     {
+        icon: Rock,
+        value: 3,
+        color: '',
+        name: 'icon3',
+        from: '#DC2E4E',
+        to:  '#9C1632'
+    },
+  ]
+
+export const gameIcons2 = [
+    {
+        icon: Scissors ,
+        value: 2,
+        color: '',
+        name: 'levelIcon1',
+        from: '#4865F4',
+        to: '#1F3CB8'
+    },
+     {
+        icon:   Spock,
+        value: 5,
+        color: '',
+        name: 'levelIcon2',
+        from:  '#42BCD0',
+        to: '#2D91A9'
+    },
+     {
+        icon: Paper,
+        value: 1,
+        color: '',
+        name: 'levelIcon3',
+        from: '#EDAA0E',
+        to: '#C76B1B'
+    },
+     {
+        icon: Lizard,
+        value: 4,
+        color: '',
+        name: 'levelIcon4',
+        from: '#834FE4',
+        to: '#5A37AA'
+    },
+     {
+        icon: Rock ,
+        value: 3,
+        color: '',
+        name: 'levelIcon5',
+         from: '#DC2E4E',
+        to:  '#9C1632'
+    },
+  ]
