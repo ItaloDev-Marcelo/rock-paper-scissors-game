0e6322069b1b24e1586a0b51c64b6fbb8459e7f6
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-01 15:54:28 -0300

    feat: creating score component

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,22 +1,18 @@
 import { logo_lv1, logo_lv2 } from "../assets/layout_images";
-
-interface ScoreType {
-    ScoreResult: number;
-    level: number;
-}
+import type { ScoreType } from "../shared/types/RulePaperType";
 
 export const Score = ({ScoreResult,level}:ScoreType) => {
 
- const selectedImage = level == 1 ? logo_lv1 : logo_lv2;
+ const selectedImage = level  ? logo_lv1 : logo_lv2;
 
   return (
-    <div className="border-1 border-Gray-header-outline flex flex-row justify-between px-1.5 h-25">
+    <div className="outline-3 rounded-md w-[95%] p-5 outline-Gray-header-outline flex flex-row justify-between  ">
         <div>
-            <img src={selectedImage} alt='Score-logo'/>
+            <img src={selectedImage} alt='Score-logo' className="w-25"/>
         </div>
-        <div>
-             <h2>Score</h2>
-             <p>{ScoreResult}</p>
+        <div className='bg-white w-20 h-20 rounded-md text-center p-2'>
+             <h2 className='uppercase text-Blue-score-text font-medium'>Score</h2>
+             <p className="text-4xl mt-1 font-semibold text-Navy-dark-text opacity-80">{ScoreResult}</p>
         </div>
     </div>
   )
