7ce0574edeb17f31e5e76fbc82f116258c9f9f0b
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-02 12:10:49 -0300

    refactor: add ?. to player and removing space

--- src/components/GameBtnOption.tsx
+++ src/components/GameBtnOption.tsx
@@ -1,12 +1,13 @@
 import type { Btntype } from "../shared/types/BtnType"
 
-export const GameBtnOption = ({name,from,to,icon,scale,player, value}:Btntype) => {
+export const GameBtnOption = ({name,from,to,icon,scale,player,value}:Btntype) => {
   return (
      <button id={name} className={`cursor-pointer bg-linear-to-r relative z-10 gap-10 rounded-full w-25 h-25 flex flex-col justify-center items-center hover:shadow-[0_0_0_35px_rgba(255,255,255,.04),0_0_0_70px_rgba(255,255,255,.03),0_0_0_110px_rgba(255,255,255,.02)] active:shadow-[0_0_0_35px_rgba(255,255,255,.04),0_0_0_70px_rgba(255,255,255,.03),0_0_0_110px_rgba(255,255,255,.02)]
+      
                shadow-[0_8px_0_rgba(0,0,0,.25),0_12px_20px_rgba(0,0,0,.25)] ${scale}
               `}  style={{
     backgroundImage: `linear-gradient(to right, ${from}, ${to})`,
-  }} onClick={() => player(value)} >
+  }} onClick={() => player?.(value)} >
                <div className='bg-white w-18 place-items-center place-content-center h-18 rounded-full  shadow-[inset_0_5px_0_rgba(0,0,0,.12)]'>
                 <img src={icon} alt='game icon' className='w-7' />
                </div>
